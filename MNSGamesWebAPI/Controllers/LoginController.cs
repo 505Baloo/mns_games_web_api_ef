6b52a06e2fe08786bdd0b360df7089fac25d2f2d
505Baloo <jordan.cheniclet@gmail.com>
2023-06-01 21:22:12 +0200

    Added LoginAppUserDTO, LoginController. Handles login requests

---
+++ MNSGamesWebAPI/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MNSGamesWebAPI.Models;
+using MNSGamesWebAPI.Models.DTO;
+using static BCrypt.Net.BCrypt;
+
+
+namespace MNSGamesWebAPI.Controllers
+{
+    [Route("api/Login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly MNS_Games_DBContext _context;
+
+        public LoginController(MNS_Games_DBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> LogAppUser(LoginAppUserDTO loginAppUserDTO)
+        {
+            if (_context.AppUsers == null)
+            {
+                return Problem("Entity set 'MNS_Games_DBContext.AppUsers' is null.");
+            }
+
+            var temp = _context.AppUsers.Where(appUser => appUser.LoginNickname == loginAppUserDTO.LoginNickname || appUser.Email == loginAppUserDTO.Email).FirstOrDefault();
+            AppUser appUserToReturn = null;
+            
+
+            if (temp == null)
+                return BadRequest("User not found!");
+            else
+            {
+                var tempPassword = temp.LoginPassword;
+                if(Verify(loginAppUserDTO.LoginPassword, tempPassword))
+                {
+                    appUserToReturn = temp;
+                }
+            }
+
+            return Ok(appUserToReturn);
+        }
+    }
+}
---
+++ MNSGamesWebAPI/Models/DTO/LoginAppUserDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace MNSGamesWebAPI.Models.DTO
+{
+    public class LoginAppUserDTO
+    {
+        public string? LoginNickname { get; set; }
+        public string LoginPassword { get; set; } 
+        public string? Email { get; set; } 
+
+        public LoginAppUserDTO() { }
+
+        public LoginAppUserDTO(AppUser appUser)
+        {
+            LoginNickname = appUser.LoginNickname;
+            LoginPassword = appUser.LoginPassword;
+            Email = appUser.Email;
+        }
+
+        public AppUser ToAppUser()
+        {
+            return new AppUser
+            {
+                LoginNickname = LoginNickname,
+                LoginPassword = LoginPassword,
+                Email = Email
+            };
+        }
+    }
+}
